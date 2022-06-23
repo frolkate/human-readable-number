3e8b6ffb04a2ce25c36ab45feafeabae93eb70ed
Kate Frolova <frolkate.alex@gmail.com>
2022-06-23 17:19:57 +0300

    fix: add check eight toReadable

--- src/index.js
+++ src/index.js
@@ -1,57 +1,73 @@
 module.exports = function toReadable (number) {
+  let wordsNumber = ["one", "two", "three", "four", "five", "six", "seven", "eight", "nine", "ten"];
+  let exeptNumber = ["", "twen", "thir", "for", "fif", "", "","eigh"];
+  
+  let remainderDivision_100 = number % 100;
+  let hundredNumber = number - remainderDivision_100; 
+  let simpleNumber  = remainderDivision_100 % 10;
+  let tenNumber =  (remainderDivision_100 - simpleNumber) / 10;
 
-        let wordsNumber = ["one", "two", "three", "four", "five", "six", "seven", "eight", "nine", "ten"];
-        let exeptNumber = ["", "twen", "thir", "for", "fif", "", "","eigh"];
-        
-        let remainderDivision_100 = number % 100;
-        let hundredNumber = number - remainderDivision_100; 
-        let simpleNumber  = remainderDivision_100 % 10;
-        let tenNumber =  remainderDivision_100 - simpleNumber;
+  let isHundred = false;
+  let isTen = false;
+  let isSimple =  false;
+  
+  let stringNumber = "";
 
+  if(hundredNumber != 0)
+  {
+    for (i = 0; i < wordsNumber.length; i ++)
+    {
+      if (hundredNumber / 100 === i &&  i != 0 && isHundred === false) {
+        isHundred = true;
+        stringNumber += `${wordsNumber[i - 1]} hundred `;
+      }
+    }
+  }
 
-        let isHundred = false;
-        let isTen = false;
-        let isSimple =  false;
+    for (i = 0; i < wordsNumber.length; i ++)
+  {
+      
+      if (tenNumber === i && i != 0 && isTen === false) {
         
-        let stringNumber = '';
+          isTen = true;
         
-        for (i = 0; i < wordsNumber.length; i ++)
-        {
-            if (hundredNumber / 100 === i &&  i != 0 && isHundred === false) {
-              isHundred = true;
-              stringNumber += `${wordsNumber[i - 1]} hundred `;
-            }
+          if (tenNumber === 2 || tenNumber === 3 || tenNumber === 4 || tenNumber === 5 || i===8)  
+          stringNumber +=  `${exeptNumber[i - 1]}ty`;
+            
+         if (tenNumber === 6 || tenNumber === 7 || tenNumber === 9)  
+        
+        stringNumber +=  `${wordsNumber[i - 1]}ty`;
+        
+        if (i=== 1 && simpleNumber === 1) {
+          isSimple = true;
+          stringNumber+= "eleven";
         }
-          for (i = 0; i < wordsNumber.length; i ++)
-        {
-            if (tenNumber / 10 === i && i != 0 && isTen === false) {
-              isTen = true;
-              if (i=== 1 && simpleNumber === 1) {
-                isSimple = true;
-                stringNumber+= "eleven";
-              }
-                     if (i=== 1 && simpleNumber === 2) {
-                isSimple = true;
-                stringNumber+= "twelve";
-              }
-                          if (i=== 1 && simpleNumber > 2) {
-                 isSimple = true;
-                 if(i === 2 || i === 3 || i === 5 || i===8)  stringNumber +=  `${exeptNumber[simpleNumber - 1]}teen`;
-                 else 
-                 stringNumber +=  `${wordsNumber[simpleNumber - 1]}teen`;
-              }
-              else{
-                if(i === 2 || i === 3 || i === 4 || i === 5 || i===8)  stringNumber +=  `${exeptNumber[i - 1]}ty`;
-                else 
-              stringNumber +=  `${wordsNumber[i - 1]}ty`;
-            }
-            }
+        
+        if (i=== 1 && simpleNumber === 2) {
+          isSimple = true;
+          stringNumber+= "twelve";
         }
-          for (i = 0; i < wordsNumber.length; i ++)
+        else { 
+           
+        if (tenNumber=== 1)
         {
-            if (simpleNumber === i && i != 0 && isSimple === false) {
-              stringNumber +=` ${wordsNumber[i - 1]}`;
-            }
+        if ( simpleNumber === 3 || simpleNumber === 5 || simpleNumber===8){                 isSimple = true;
+            stringNumber +=  `${exeptNumber[simpleNumber - 1]}teen`;
         }
-        return stringNumber;       
+        else
+        {
+           isSimple = true;
+           stringNumber +=  `${wordsNumber[simpleNumber - 1]}teen`;
+        }
+        }
+      }
+  }
+  }
+    for (i = 0; i < wordsNumber.length; i ++)
+  {
+      if (simpleNumber === i && i != 0 && isSimple === false) {
+        stringNumber +=` ${wordsNumber[i - 1]}`;
+      }
+  }
+  return stringNumber;     
 }
